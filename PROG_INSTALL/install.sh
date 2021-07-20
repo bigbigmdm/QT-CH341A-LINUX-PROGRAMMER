80acc33ccd4f50f86b8fb1ee6847efc90dde18cb
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2021-07-20 13:10:43 +0300

    Add files via upload
    It is a icons and shell script to installing the programmer.

---
+++ PROG_INSTALL/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+dir=$(pwd)
+cd ~
+if ! [ -e $HOME/PROG24 ]; then
+mkdir PROG24
+fi
+cd $dir
+cp prog24 ~/PROG24
+cp Prog24cXX.desktop ~/PROG24/
+cp 24Cxx_icon64.png ~/PROG24/
+cp 24Cxx_icon48.png ~/PROG24/
+cp 24Cxx_icon32.png ~/PROG24/
+cp 99-CH341.rules ~/PROG24/
+cd  ~/Prog24cXX/
+if [ -e $HOME/"Рабочий стол" ]; then 
+cp Prog24cXX.desktop $HOME/"Рабочий стол"/
+fi
+if [ -e $HOME/Desktop ]; then 
+cp Prog24cXX.desktop $HOME/Desktop/
+fi
+sudo cp 99-CH341.rules /etc/udev/rules.d/
+chmod 777 *
+
