8bac3a0d5e0c7d025280b01cb7bb64ae679edd94
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2021-07-21 14:12:57 +0300

    Update install.sh
    Changing the working directory

--- PROG_INSTALL/install.sh
+++ PROG_INSTALL/install.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 dir=$(pwd)
-cd ~
-if ! [ -e $HOME/PROG24 ]; then
-mkdir PROG24
+if ! [ -e /opt/PROG24 ]; then
+mkdir /opt/PROG24
 fi
 cd $dir
-cp prog24 ~/PROG24
-cp Prog24cXX.desktop ~/PROG24/
-cp 24Cxx_icon64.png ~/PROG24/
-cp 24Cxx_icon48.png ~/PROG24/
-cp 24Cxx_icon32.png ~/PROG24/
-cp 99-CH341.rules ~/PROG24/
-cd  ~/PROG24/
+cp prog24 /opt/PROG24
+cp Prog24cXX.desktop /opt/PROG24
+cp 24Cxx_icon64.png /opt/PROG24
+cp 24Cxx_icon48.png /opt/PROG24
+cp 24Cxx_icon32.png /opt/PROG24
+cp 99-CH341.rules /opt/PROG24
+cd  /opt/PROG24
 if [ -e $HOME/"Рабочий стол" ]; then 
-cp Prog24cXX.desktop $HOME/"Рабочий стол"/
+cp Prog24cXX.desktop $HOME/"Рабочий стол"
 fi
 if [ -e $HOME/Desktop ]; then 
-cp Prog24cXX.desktop $HOME/Desktop/
+cp Prog24cXX.desktop $HOME/Desktop
 fi
-sudo cp 99-CH341.rules /etc/udev/rules.d/
+cp 99-CH341.rules /etc/udev/rules.d
 chmod 777 *
-
