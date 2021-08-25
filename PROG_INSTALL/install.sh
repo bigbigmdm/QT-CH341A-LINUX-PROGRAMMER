87da02aa4d4408969eec50884116df97972f8344
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2021-08-25 14:28:46 +0300

    Add files via upload
    Deleting a desktop icon copying

--- PROG_INSTALL/install.sh
+++ PROG_INSTALL/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir=$(pwd)
 userhome=$HOME
-su
+echo $userhome 
 if ! [ -e /opt/PROG24 ]; then
 mkdir /opt/PROG24
 fi
@@ -13,12 +13,6 @@ cp 24Cxx_icon48.png /opt/PROG24
 cp 24Cxx_icon32.png /opt/PROG24
 cp 99-CH341.rules /opt/PROG24
 cd  /opt/PROG24
-if [ -e $userhome/"Рабочий стол" ]; then 
-cp Prog24cXX.desktop $HOME/"Рабочий стол"
-fi
-if [ -e $userhome/Desktop ]; then 
-cp Prog24cXX.desktop $userhome/Desktop
-fi
 cp Prog24cXX.desktop /usr/share/applications
 cp 24Cxx_icon64.png /usr/share/pixmaps/
 cp 24Cxx_icon48.png /usr/share/pixmaps/
