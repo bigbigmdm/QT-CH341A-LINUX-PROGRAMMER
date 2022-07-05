1c7f21b5d97a4435e4e8ea83f0cd08e3f6992cb2
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2022-07-05 11:55:20 +0300

    Fix the bug of the user privileges

--- PROG_INSTALL/install.sh
+++ PROG_INSTALL/install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 dir=$(pwd)
-userhome=$HOME
-echo $userhome 
 if ! [ -e /opt/PROG24 ]; then
-mkdir /opt/PROG24
+sudo mkdir /opt/PROG24
 fi
 cd $dir
-cp prog24 /opt/PROG24
-cp Prog24cXX.desktop /opt/PROG24
-cp 24Cxx_icon64.png /opt/PROG24
-cp 24Cxx_icon48.png /opt/PROG24
-cp 24Cxx_icon32.png /opt/PROG24
-cp 99-CH341.rules /opt/PROG24
+sudo cp prog24 /opt/PROG24
+sudo chmod u+x /opt/PROG24/prog24
+sudo cp Prog24cXX.desktop /opt/PROG24
+sudo cp 24Cxx_icon64.png /opt/PROG24
+sudo cp 24Cxx_icon48.png /opt/PROG24
+sudo cp 24Cxx_icon32.png /opt/PROG24
+sudo cp 99-CH341.rules /opt/PROG24
 cd  /opt/PROG24
-cp Prog24cXX.desktop /usr/share/applications
-cp 24Cxx_icon64.png /usr/share/pixmaps/
-cp 24Cxx_icon48.png /usr/share/pixmaps/
-cp 24Cxx_icon32.png /usr/share/pixmaps/
-cp 99-CH341.rules /etc/udev/rules.d
-chmod 777 *
+sudo cp Prog24cXX.desktop /usr/share/applications
+if [ -e $HOME/"Рабочий стол" ]; then 
+cp Prog24cXX.desktop $HOME/"Рабочий стол"
+fi
+if [ -e $HOME/Desktop ]; then 
+cp Prog24cXX.desktop $HOME/Desktop
+fi
+sudo cp 99-CH341.rules /etc/udev/rules.d
+sudo chmod 777 *
